8adb379b9f59af36086ffaa599b73774bb5e617f
Andrew Dillon <tajd1@unigroupinc.com>
2019-02-07 07:56:16 -0600

    Add Call Stack decription

--- src/components/CallStackDescription.js
+++ src/components/CallStackDescription.js
@@ -2,6 +2,8 @@
 import React from 'react';
 
 import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -24,42 +26,23 @@ const CallStackDescription = ({
     <DialogTitle id="scroll-dialog-title">About the Call Stack</DialogTitle>
     <DialogContent>
       <DialogContentText>
-        Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac
-        facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum
-        at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus
-        sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum
-        nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur
-        et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras
-        mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
-        egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-        lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
-        sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-        Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
-        consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
-        egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-        lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
-        sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-        Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
-        consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
-        egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-        lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
-        sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-        Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
-        consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
-        egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-        lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
-        sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-        Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
-        consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
-        egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-        lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
-        sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-        Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+        The Call Stack is a fundamental part of the JavaScript language. It is a record-keeping structure that allows us to perform function calls. Each function call is represented as a frame on the Call Stack. This is how the JavaScript engine keeps track of which functions have been called and in what order. The JS engine uses this information to ensure execution picks back up in the right spot after a function returns.
+      </DialogContentText>
+      <br />
+      <DialogContentText>
+        When a JavaScript program first starts executing, the Call Stack is empty. When the first function call is made, a new frame is pushed onto the top of the Call Stack. When that function returns, its frame is popped off of the Call Stack.
+      </DialogContentText>
+      <br />
+      <DialogContentText>
+        <Link
+          variant="body2"
+          color="secondary"
+          href="https://www.ecma-international.org/ecma-262/9.0/index.html#sec-execution-contexts"
+          target="_blank"
+          rel="noreferrer"
+         >
+          Learn more from the JavaScript Spec
+         </Link>
       </DialogContentText>
     </DialogContent>
     <DialogActions>
